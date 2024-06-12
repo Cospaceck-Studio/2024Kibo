bae51ccac88b9ba7046e0190cbd56f45c740fd7f
Matthew Tseng <93825287+Mattzm2021@users.noreply.github.com>
2024-06-12 20:25:20 +0800

    Hi

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -2,10 +2,6 @@ package jp.jaxa.iss.kibo.rpc.defaultapk;
 
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
-/**
- * Class meant to handle commands from the Ground Data System and execute them in Astrobee
- */
-
 public class YourService extends KiboRpcService {
     @Override
     protected void runPlan1(){
